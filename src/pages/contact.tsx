1260c779f08589f1510eb6a426675312255262b8
Jono Cairns <jonocairns@gmail.com>
2019-07-21 15:05:58 +1200

    hidden inputs

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -49,12 +49,13 @@ const Contact = (props: Props) => {
 
         {!isSuccess && (
           <form
-            name="contact"
             action="/contact?success"
             method="post"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
           >
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="contact" />
             <div className="form-group">
               <label>Email address</label>
               <input
