e2d01e7ac9476d068b5104c2c5ab4eb5e829add1
Jono Cairns <jonocairns@gmail.com>
2020-05-09 13:52:11 +1200

    fix thing

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -12,16 +12,15 @@ export const Hero = (props: Props) => {
   const {image, title, lead} = props;
 
   return (
-    <div className="jumbotron jumbotron-fluid pb-0 mb-0" style={{
+    <div className="jumbotron jumbotron-fluid py-5 my-0" style={{
         backgroundImage: `url(${img})`,
         backgroundPosition: '50% 0%',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
-        minHeight: '400px',
         backgroundAttachment: 'fixed'}}>
     <div className="container">
-    <h1 className="display-4 text-white">{title}</h1>
-    <p className="lead text-white" style={{maxWidth: '500px'}}>{lead}</p>
+    <h1 className="display-4 text-white pt-5">{title}</h1>
+    <p className="lead text-white pb-5" style={{maxWidth: '500px'}}>{lead}</p>
     </div>
     </div>
   );
